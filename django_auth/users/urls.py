bca679ec8f6682644b460cf14b3397ae44e27f80
Sergiy <sergiy.homenko@gmail.com>
2019-07-22 13:14:24 +0300

    fix create user

--- django_auth/users/urls.py
+++ django_auth/users/urls.py
@@ -1,9 +1,9 @@
-from . import views
+from .views import CreateUserAPIView, authenticate_user, UserRetrieveUpdateAPIView, current_user
 from django.urls import path
  
 urlpatterns = [
-    path('create/', views.CreateUserAPIView),
-    path('auth/', views.authenticate_user),
+    path('create/', CreateUserAPIView.as_view()),
+    path('auth/', authenticate_user),
     #path('update/', views.UserRetrieveUpdateAPIView),
-    path('get_current/', views.current_user),
+    path('get_current/', current_user),
 ]
